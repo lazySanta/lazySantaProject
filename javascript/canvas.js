dfe5f40bb327b892a7f08319a7af295cfa87d3c2
Magda_J <jarkiewicz.magda@gmail.com>
2019-11-30 14:06:55 +0000

    Santa is moving! (and with a background :-o )

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,41 +1,92 @@
-var state = {
-  positions: [],
-};
-
 var canvas = document.querySelector("#screen")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
 
-function clearCanvas(e) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
-}
+var state = {
+  santa: {
+    x: canvas.width / 2,
+    y: canvas.height *2/3,
+  },
+  presents: {
+    x: 100,
+    y: 50,
+    speed: 5,
+  }
+};
 
-
-var x=50;
-var y=350;
-
-function draw (ctx,image) {
-  if (!image.complete) {
+function drawBackground (ctx,background) {
+  if (!background.complete) {
     setTimeout(function(){
-      draw(ctx,image);
-    }, 100)
+      drawBackground(ctx,background);
+    }, 150)
     return
   }
-  ctx.drawImage(image, x, y, 100, 100);
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+}
+
+var background = new Image();
+background.src = '../lazySantaProject/images/elements/background_web.png'
+
+// function clearCanvas(e) {
+//     ctx.fillStyle = "MidnightBlue";
+//     ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+// }
+
+function drawSanta (ctx,image) {
+  if (!image.complete) {
+    setTimeout(function(){
+      drawSanta(ctx,image);
+    }, 150)
+    return
+  }
+  ctx.drawImage(image, state.santa.x, state.santa.y, 150, 150);
 }
 
 var image = new Image();
-image.src = '../lazySantaProject/images/temporarySanta.jpg'
-draw(ctx,image);
+image.src = '../lazySantaProject/images/elements/santa_catching.png'
+
+
+//Santa moving with arrow keys
 
 function santaMoving(e) {
-    if (e.code==="ArrowLeft") {x-=5;
-    } else if(e.code==="ArrowRight") {x+=5;
+    if (e.code==="ArrowLeft") {state.santa.x-=5;
+    } else if(e.code==="ArrowRight") {state.santa.x+=5;
     }
-    clearCanvas();
-    draw(ctx,image);
+    drawBackground();
+    drawSanta(ctx,image);
   };
 body.addEventListener("keydown", santaMoving);
+
+// draw Presents
+
+var presentImage = new Image()
+presentImage.src = '../lazySantaProject/images/elements/present_pink.png'
+
+
+function drawPresent (ctx, presentImage) {
+  if (!presentImage.complete) {
+    setTimeout(function(){
+      drawPresent(ctx,presentImage);
+    }, 100)
+    return
+  }
+  ctx.drawImage(presentImage, state.presents.x, state.presents.y, 50, 50);
+}
+drawPresent(ctx,presentImage);
+//Presents falling
+
+function presentFall () {
+    state.presents.y+=state.presents.speed
+};
+
+
+function animate() {
+    drawBackground(ctx, background);
+    drawPresent(ctx, presentImage);
+    drawSanta(ctx, image);
+    presentFall();
+};
+
+setInterval(animate, 50);
